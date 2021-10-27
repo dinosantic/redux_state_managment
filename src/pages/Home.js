cc4455e160ed1da8b869d954976f1a33da09b15b
Dino Santic <dinos.mos@gmail.com>
2021-10-27 12:17:14 +0200

    Rendering data and initial styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,49 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+//Components
+import Game from "../components/Game";
+//Styling and animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+  //
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
+
+  console.log(popular);
   return (
-    <div>
-      <h1></h1>
-    </div>
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+    </GameList>
   );
 };
 
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 2rem 0 4rem;
+  }
+`;
+//
+const Games = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 2rem;
+`;
 export default Home;
